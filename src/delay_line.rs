adf58f200fca343074ec529003f769dc77fd1c01
Paul Adenot <paul@paul.cx>
2019-08-19 07:41:10 +0200

    make the delay line code shorter

--- src/delay_line.rs
+++ src/delay_line.rs
@@ -33,24 +33,14 @@ impl DelayLine {
         } else {
             self.write_index - duration
         };
-        // delay=1000
-        // size=48000
-        // write=0 % 1000=0
-        // read=48000 - write - duration = 47000
     }
     pub fn write(&mut self, input: f32) {
-        let mut w = self.write_index;
-        let l = self.duration;
-        self.memory[w] = input;
-        w = (w + 1) % l;
-        self.write_index = w;
+        self.memory[self.write_index] = input;
+        self.write_index = (self.write_index + 1) % self.duration;
     }
     pub fn read(&mut self, output: &mut f32) {
-        let mut r = self.read_index;
-        let l = self.duration;
-        *output = self.memory[r];
-        r = (r + 1) % l;
-        self.read_index = r;
+        *output = self.memory[self.read_index];
+        self.read_index = (self.read_index + 1) % self.duration;
     }
     pub fn process(&mut self, input: f32, output: &mut f32) {
         self.write(input);
